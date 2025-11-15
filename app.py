dafbc506584fb9843142e4a751ac9b55052ef8be
SENAC\gustavo60225237 <gustavo.6068@df.senac.br>
2025-11-14 22:38:51 -0300

    Iniciando sistema de login com session usando flask

--- app.py
+++ app.py
@@ -1,30 +1,62 @@
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = 'MINHA CHAVE SECRETA'
 
 @app.get('/')
-def home():
-    teste = request
-    a = 10
-    b = 20
-    c = a + b
-    return f"A soma é {c}"
+def paginaInicial():
+    if 'nomeUsuarioLogado' in session:
+        return render_template('pagina_usuario_logado.html', nomeUsuario=session['nomeUsuarioLogado'])
+    else:
+        return redirect("/login")
 
 @app.get('/login')
-def login():
-    return render_template('login.html')
+def exibirPaginaLogin():
+    return render_template('login.html', msgErro='')
 
 
 @app.post('/login')
 def realizarLogin():
+    usuarios = [
+        {'email': 'teste@teste.com', 'password': '123456', 'nome': 'Teste'},
+        {'email': 'camila@teste.com', 'password': 'abcdef', 'nome': 'Camila'},
+        {'email': 'joao@teste.com', 'password': 'qwerty', 'nome': 'João'}
+    ]
+
     email = request.form.get('email')
     password = request.form.get('password')
-    if(email == 'teste@teste.com' and password == '123456'):
-        return "Login realizado com sucesso!"
-    else:
-        return "Falha no login."
 
+    nomeUsuarioEncontrado = None
+
+    for u in usuarios:
+        if u['email'] == email and u['password'] == password:
+            nomeUsuarioEncontrado = u['nome']
+            break
+
+    if nomeUsuarioEncontrado is not None:
+        session['nomeUsuarioLogado'] = nomeUsuarioEncontrado
+        return render_template('pagina_usuario_logado.html', nomeUsuario=nomeUsuarioEncontrado)
+    else:
+        return render_template('login.html', msgErro='Usuário ou senha inválidos. Tente novamente.')
+
+@app.get('/logout')
+def realizarLogout():
+    session.pop('nomeUsuarioLogado', None)
+    return "Logout realizado com sucesso!"
+
+def validarAutenticacaoUsuario():
+    return ('nomeUsuarioLogado' in session)
+
+@app.get('/pagina_usuario_logado')
+def paginanomeUsuarioLogado():
+    if validarAutenticacaoUsuario() == True:
+        return render_template('pagina_usuario_logado.html', nomeUsuario=session['nomeUsuarioLogado'])
+    else:
+        return redirect("/login")
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
